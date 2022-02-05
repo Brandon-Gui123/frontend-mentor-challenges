9ad4073ce8b170f19ef5cc91d75a8efebcf0dcb1
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-05 23:54:11 +0800

    Add thematic changes to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Outfit, Verdana, Geneva, Tahoma, sans-serif'
+      }
+    },
+    colors: {
+      'white': 'hsl(0, 0%, 100%)',
+      'light-gray': 'hsl(212, 45%, 89%)',
+      'grayish-blue': 'hsl(220, 15%, 55%)',
+      'dark-blue': 'hsl(218, 44%, 22%)'
+    },
   },
   plugins: [],
 }
