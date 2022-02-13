0ce2756ca54b37f9e1082887602cbf911cf55439
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-13 21:36:15 +0800

    Define some colours in Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'soft-blue': 'hsl(215, 51%, 70%)',
+      'cyan': 'hsl(178, 100%, 50%)',
+      'white': 'hsl(0, 0%, 100%)'
+    },
   },
   plugins: [],
 }
