bff57aa6b7f2b18e5019d7f3074cd18f3e3a36bc
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-13 21:47:27 +0800

    Define font weights in Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       'cyan': 'hsl(178, 100%, 50%)',
       'white': 'hsl(0, 0%, 100%)'
     },
+    fontWeight: {
+      'light': 300,
+      'normal': 400,
+      'bold': 600
+    }
   },
   plugins: [],
 }
