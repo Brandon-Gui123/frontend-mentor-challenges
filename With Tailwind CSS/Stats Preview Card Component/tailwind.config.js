8a359200794e5e11ebf5b6b6059c299eb22f288e
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-18 01:23:32 +0800

    Add font family config in Tailwind config file
    I don't intend to define a property for the Lexend Deca font. This font
    is only used in the 3 labels at the bottom of the stats preview
    component and it might be easier to
    use Tailwind CSS's ability to use arbitrary values.

--- With Tailwind CSS/Stats Preview Card Component/tailwind.config.js
+++ With Tailwind CSS/Stats Preview Card Component/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "sans": ["Inter", ...defaultTheme.fontFamily.sans]
+    }
   },
   plugins: [],
 }
