b4e08dfe7245a448dc3fde1f6e699fa7fab87c6d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-19 19:28:44 +0800

    Add white colour to Tailwind config file

--- With Tailwind CSS/Profile Card Component/tailwind.config.js
+++ With Tailwind CSS/Profile Card Component/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       "dark-cyan": "hsl(185, 75%, 39%)",
       "very-dark-desaturated-blue": "hsl(229, 23%, 23%)",
       "dark-grayish-blue": "hsl(227, 10%, 46%)",
-      "dark-gray": "hsl(0, 0%, 59%)"
+      "dark-gray": "hsl(0, 0%, 59%)",
+      "white": "hsl(0, 0%, 100%)"
     },
     fontFamily: {
       "sans": ["Kumbh Sans", ...defaultTheme.fontFamily.sans]
