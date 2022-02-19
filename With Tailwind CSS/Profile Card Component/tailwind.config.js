0b7cecfceffdbfd865d468616eb99744ecfd2b32
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-19 18:26:07 +0800

    Add colours to Tailwind config file

--- With Tailwind CSS/Profile Card Component/tailwind.config.js
+++ With Tailwind CSS/Profile Card Component/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "dark-cyan": "hsl(185, 75%, 39%)",
+      "very-dark-desaturated-blue": "hsl(229, 23%, 23%)",
+      "dark-grayish-blue": "hsl(227, 10%, 46%)",
+      "dark-gray": "hsl(0, 0%, 59%)"
+    }
   },
   plugins: [],
 }
