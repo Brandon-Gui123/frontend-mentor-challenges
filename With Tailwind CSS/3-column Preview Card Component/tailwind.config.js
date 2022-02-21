a1371fab75f49ff960fdf90ba815f63f38eee190
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-21 15:46:59 +0800

    Set up fonts in Tailwind config file

--- With Tailwind CSS/3-column Preview Card Component/tailwind.config.js
+++ With Tailwind CSS/3-column Preview Card Component/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
@@ -8,6 +10,10 @@ module.exports = {
       'very-dark-cyan': 'hsl(179, 100%, 13%)',
       'transparent-white': 'hsla(0, 0%, 100%, 0.75)',
       'very-light-gray': 'hsl(0, 0%, 95%)'
+    },
+    fontFamily: {
+      'sans': ['Lexend Deca', ...defaultTheme.fontFamily.sans],
+      'display': ['Big Shoulders Display', ...defaultTheme.fontFamily.sans] // Fallback to sans font family if this display font fails to load
     }
   },
   plugins: [],
