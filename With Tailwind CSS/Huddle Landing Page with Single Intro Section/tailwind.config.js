9d2bf897d8494a3268ecd38a8aa2afe266095fa5
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-22 12:17:23 +0800

    Add Open Sans to Tailwind config file

--- With Tailwind CSS/Huddle Landing Page with Single Intro Section/tailwind.config.js
+++ With Tailwind CSS/Huddle Landing Page with Single Intro Section/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
@@ -6,6 +8,9 @@ module.exports = {
       "violet": "hsl(257, 40%, 49%)",
       "soft-magenta": "hsl(300, 69%, 71%)",
       "white": "#ffffff"
+    },
+    fontFamily: {
+      "sans": ["Open Sans", ...defaultTheme.fontFamily.sans]
     }
   },
   plugins: [],
