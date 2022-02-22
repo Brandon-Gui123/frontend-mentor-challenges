2454160811284f9c3b35554a5a1afbd1d9c970ff
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-02-22 12:05:59 +0800

    Set up colours in Tailwind config file

--- With Tailwind CSS/Huddle Landing Page with Single Intro Section/tailwind.config.js
+++ With Tailwind CSS/Huddle Landing Page with Single Intro Section/tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "violet": "hsl(257, 40%, 49%)",
+      "soft-magenta": "hsl(300, 69%, 71%)",
+      "white": "#ffffff"
+    }
   },
   plugins: [],
 }
