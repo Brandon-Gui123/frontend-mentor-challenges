122654e52246cfa96d7962b18d0ddb26f23d67ed
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-03-06 17:59:29 +0800

    Add white to list of colours in Tailwind config

--- With Tailwind CSS/Single Price Grid Component/tailwind.config.js
+++ With Tailwind CSS/Single Price Grid Component/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       "cyan": "hsl(179, 62%, 43%)",
       "bright-yellow": "hsl(71, 73%, 54%)",
       "light-gray": "hsl(204, 43%, 93%)",
-      "grayish-blue": "hsl(218, 22%, 67%)"
+      "grayish-blue": "hsl(218, 22%, 67%)",
+      "white": "#ffffff"
     },
     fontFamily: {
       "sans": ["Karla", ...defaultTheme.fontFamily.sans]
