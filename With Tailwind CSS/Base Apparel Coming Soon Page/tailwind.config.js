1f7b22bf9cb9aa2f888a45aafab3f49fd9296f9e
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-03-09 12:53:04 +0800

    Set up colours to use in Tailwind

--- With Tailwind CSS/Base Apparel Coming Soon Page/tailwind.config.js
+++ With Tailwind CSS/Base Apparel Coming Soon Page/tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      // Primary
+      "desaturated-red": "hsl(0, 36%, 70%)",
+      "soft-red": "hsl(0, 93%, 68%)",
+
+      // Neutral
+      "dark-grayish-red": "hsl(0, 6%, 24%)",
+
+      // Common
+      "white": "hsl(0, 0%, 100%)",
+      "dark-gray": "hsl(330, 7%, 6%)"
+    }
   },
   plugins: [],
 }
