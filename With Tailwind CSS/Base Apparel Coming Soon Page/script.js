1c35bee5f3f13602dc1f5d9f2837521969939975
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-03-10 13:01:16 +0800

    Add script to style elements based on input

---
+++ With Tailwind CSS/Base Apparel Coming Soon Page/script.js
@@ -0,0 +1,16 @@
+var emailFieldErrorIcon = document.getElementById("email-field-error-icon");
+var emailFieldErrorText = document.getElementById("email-field-error-text");
+
+function emailFieldOnInput(emailFieldElement)
+{
+    if (emailFieldElement.validity.valid)
+    {
+        emailFieldErrorIcon.style.display = "none";
+        emailFieldErrorText.style.display = "none";
+    }
+    else
+    {
+        emailFieldErrorIcon.style.display = "block";
+        emailFieldErrorText.style.display = "block";
+    }
+}
