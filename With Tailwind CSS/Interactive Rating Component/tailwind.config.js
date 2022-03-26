7f7aa2d314eb48ea7fe79d7d3225cfcc10a03f76
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2022-03-26 17:51:57 +0800

    Set up colours to use for the site

--- With Tailwind CSS/Interactive Rating Component/tailwind.config.js
+++ With Tailwind CSS/Interactive Rating Component/tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      // primary
+      "orange": "hsl(25, 97%, 53%)",
+
+      // neutral
+      "white": "hsl(0, 0%, 100%)",
+      "light-grey": "hsl(217, 12%, 63%)",
+      "medium-grey": "hsl(216, 12%, 54%)",
+      "dark-blue": "hsl(213, 19%, 18%)",
+      "very-dark-blue": "hsl(216, 12%, 8%)"
+    }
   },
   plugins: [],
 }
